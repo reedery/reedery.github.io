8bbae2261fad33b48408d0581fc029483e407b18
Ryan Reede <reedery>
2024-05-08 15:23:29 -0700

    Content updates

--- js/projectData.js
+++ js/projectData.js
@@ -12,7 +12,7 @@ export const projectData = [
       "Product Designer",
       "Co-founder",
     ],
-    "description": "Vortex was the first B2C product we built at Teleportal (the startup I co-founded/built from 2019-2024). It was based on the networking stack from the Teleportal SDK but focused on the problems video creators face when looking to use the medium of 3D Animation for the first time. In addition to performing object motion via touch-screen input, Vortex allows you to use your mobile device's AR tracking to animate. Inspired by Mandalorian-style virtual production, Figma, multitrack recorders and music/video editors, Vortex aimed to make CGI accessible to any mobile creator. In many ways we were succeeded, but ultimately we would pivot to Movieoke for a better UX. <a href='https://medium.com/teleportal/made-in-vortex-166daa63c074' target=\"_blank\">Read more about Vortex here.</a>",
+    "description": "Vortex was the first B2C product we built at Teleportal (the startup I co-founded/built from 2019-2024). It was based on the networking stack from the Teleportal SDK but focused on the problems video creators face when looking to use the medium of 3D Animation for the first time. Our team of four released 11 version  In addition to performing object motion via touch-screen input, Vortex allows you to use your mobile device's AR tracking to animate. Inspired by Mandalorian-style virtual production, Figma, multitrack recorders and music/video editors, Vortex aimed to make CGI accessible to any mobile creator. In many ways we were succeeded, but ultimately we would pivot to Movieoke for a better UX. <a href='https://medium.com/teleportal/made-in-vortex-166daa63c074' target=\"_blank\">Read more about Vortex here.</a>",
     "video": [
         ["Tutorial of basic concepts in Vortex", "https://streamable.com/e/89glmc"],
         ["Recording and exporting in Vortex", "https://www.youtube.com/embed/DOWbraISi-o?start=20"],
@@ -31,25 +31,26 @@ export const projectData = [
       "Business Development",
       "Co-founder"
     ],
-    "description": "This was the first product Teleportal (the startup I co-founded/built from 2019-2024) built and released. The Teleportal SDK worked across platforms (iOS / Android / VR / PC / Mac) and interfaces with the Unity game engine. The Teleportal SDK would seamlessly synchronize user locations and coordinate systems between devices and automatically save and retrieve persistent objects in the shared space. The Teleportal SDK integrated with existing Unity projects and three.js.<br><br><a href='https://medium.com/teleportal/announcing-teleportal-the-xr-telepresence-platform-350a710c4c0a' target=\"_blank\">Read more about Teleportal here</a> and view the archived <a href=\"https://web.archive.org/web/20200202004243/https://teleportal.dev/docs/\" target=\"_blank\">Developer Docs</a>.",
+    "description": "This was the first product Teleportal (the startup I co-founded/built from 2019-2024) built and released. The Teleportal SDK worked across platforms (iOS / Android / VR / PC / Mac) and interfaced with the Unity game engine and Three.js. The Teleportal SDK would seamlessly synchronize user locations and coordinate systems between devices and automatically save and retrieve persistent objects in the shared space. It was built around a modular and extensible service-oriented architecture in Typescript. My main responsibility was the SDK interface code and improving the developer experience in Unity with each release. Our partners built multiplayer games for Magic Leap, real-time multiuser art exhibits and virtual production software.<br><br><a href='https://medium.com/teleportal/announcing-teleportal-the-xr-telepresence-platform-350a710c4c0a' target=\"_blank\">Read more about Teleportal here</a> and view the archived <a href=\"https://web.archive.org/web/20200202004243/https://teleportal.dev/docs/\" target=\"_blank\">Developer Docs</a>.",
     "video": [
         ["Getting started with the Teleportal SDK", "https://www.youtube.com/embed/jp__SVtPbdQ?start=217"]
     ],
     "flickrAlbumUrl": ""
   },
   {
-    "title": "Jurassic World",
+    "title": "Jurassic World VR",
     "photo": "/images/project/jurassic.jpeg",
     "year": 2018,
     "details": "UE4/Motion Simulator (HTC Vive)",
-    "subtitle": "Jurassic World: VR Expedition was a location-based VR motion-simulator ride powered by UE4 at Dave & Buster's. It was built by our team at VRC in collaboration with Universal with dinosaur assets built by ILM.",
+    "subtitle": "Jurassic World: VR Expedition was a location-based VR motion-simulator ride powered by UE4 at Dave & Buster's. It was built by our team at VRC in collaboration with Universal for the film release with dinosaur/environment assets built by ILM.",
     "role": [
       "Software Engineer",
       "Pipeline Technical Director"
     ],
-    "description": "Jurassic VR immersed players in an off-roading adventure through the Jurassic World park where they must tag dinosaurs with a laser gun. Each of the four players wear HTC Vive headsets, and is strapped into a four-person motion simulator platform that moves on 3 axis. The experience was 4 min and costed $5 per person, quickly making it one of the most successful VR location-based experiences available.<br><br>The ride was built with a modified version of UE4 that prioritized data moving between the game engine server that was hosting the session for all four players and the motion simulator hardware platform. In addition to gameplay programming in Blueprints, I was responsible for building in-engine tools for rendering nightlys (360 stereo videos) that our director could review each morning and see the latest build. I also scripted our pipeline from Maya to Unreal with axis/unit scale conversions so  animators could push their changes directly to Perforce from Maya.",
+    "description": "Jurassic VR immersed players in an off-roading adventure through the Jurassic World park where they must tag dinosaurs with a laser gun. Each of the four players wear HTC Vive headsets, and is strapped into a four-person motion simulator platform that moves on 3 axis. The experience was 4 min and costed $5 per person, quickly making it one of the most successful VR location-based experiences available.<br><br>The ride was built with a modified version of UE4 that prioritized data moving between the game engine server that was hosting the session for all four players and the motion simulator hardware platform. In addition to gameplay programming in Blueprints, I was responsible for building in-engine tools for rendering nightlys (360 stereo videos) that our director could review each morning and see the latest build. I also scripted our pipeline from Maya to Unreal with axis/unit scale conversions so  animators could push their changes directly to Perforce from Maya.<br><br>Read more <a href=\"https://variety.com/2018/digital/news/jurassic-world-vr-expeditions-dave-busters-1202831448/\" target=\"_blank\">about the experience on Variety</a>.",
     "video": [
-        ["Video review of the Jurassic World VR Experience", "https://www.youtube.com/embed/YjsfLltva4s?start=120"]
+        ["Video review of the Jurassic World VR Experience", "https://www.youtube.com/embed/YjsfLltva4s?start=120"],
+        ["Video of the final installation in Dave and Busters.", "https://www.youtube.com/embed/rEH-tEfG3t4?start=96"]
     ],
     "flickrAlbumUrl": "https://www.flickr.com/photos/200636511@N06/albums/72177720316789279"
   },
@@ -64,7 +65,7 @@ export const projectData = [
     ],
     "description": "At GDC 2019, I gave a speech entitled \"VR and Digital Humanities\" where I presented our Joycestick VR project (view the Joycestick project for more info) to an audience of developers and educators.<br><br>I spoke about how we adapted a book (<em>Ulysses</em>) for VR, how our product management process worked to deliver the project with a team of 30 students and takeaways on how to improve outcomes for students doing similar experiences with VR.",
     "video": [
-        ["GDC Speech on Joycestick", "https://www.youtube.com/embed/f3Z1c9QdFn4?start=661"]
+        ["GDC Speech on Joycestick", "https://www.youtube.com/embed/f3Z1c9QdFn4?start=1016"]
     ],
     "flickrAlbumUrl": "https://www.flickr.com/photos/200636511@N06/albums/72177720316768787"
   },
@@ -73,14 +74,14 @@ export const projectData = [
     "photo": "../images/project/movieoke.jpg",
     "year": 2022,
     "details": "Unity 3D App (iOS + Android)",
-    "subtitle": "Movieoke (Movie Karaoke) was a product for creating 3D animated remakes of iconic moments from movies and memes. It was built at Teleportal from 2021-23 and scaled to 320k users worldwide.",
+    "subtitle": "Movieoke (Movie Karaoke) was a product for creating 3D animated remakes of iconic moments from movies and memes. We scaled our app to 320k users within our target demographic of teenage video creators.",
     "role": [
       "Software Engineer",
       "Technical Artist",
       "Product Manager",
       "Co-founder"
     ],
-    "description": "At Teleportal (the startup I co-founded/built from 2019-2023), democratizing 3D content creation was core to our vision. After iterating on Vortex (project info on this site) for 11 releases, it was clear from the analytics that the product was too difficult to use and the product did little to help creators overcome the blank canvas problem.<br><br>Our team came up with Movie Karaoke as a fun way to reimagine storytelling with 3D Animated characters on a mobile device by putting facial capture and voice recording at the center of the UX. The UI was designed like Snapchat’s camera and having scene templates with a teleprompter made for creators to get started. A team of 5 artists/engineers built and released 20 Movieoke versions from April ’22 to March ’23 (at which point Movieoke became MovieBot). The 2.0 release was notable: it introduced customizable scenes and significantly improved the user funnel (51% more recordings, 17% more exports).<br><br>My role during this time was multifaceted. Externally I was responsible for investor updates and internally, I was the product manager working with our lead engineer and designer on scoping eng/tech art sprints and planning upcoming releases. I would pick up art and engineering tasks as needed to ensure that release schedules were met. Once a release was tested and passed QA, I also managed release engineering and publishing updates to Google Play/App Store each two weeks. In 7 months, we scaled from just an idea to 25k WAU/4k DAU on 322k total installs with an average rating of 4.6 starts on iOS/Android.<br><br><a href=\"https://medium.com/teleportal/now-playing-movieoke-2285d02cfa42\" target=\"_blank\">More information here</a>.",
+    "description": "At Teleportal (the startup I co-founded/built from 2019-2023), democratizing 3D content creation was core to our vision. We pivoted from Vortex (project info on this site) to Movieoke at the end of 2021 to build a better UX around our learnings and better assist creators in overcoming the blank canvas problem.<br><br>Our team came up with Movie Karaoke as a fun way to reimagine storytelling with 3D Animated characters on a mobile device by putting facial capture and voice recording at the center of the UX. The UI was designed like Snapchat’s camera and having scene templates with a teleprompter made for creators to get started. Our product team of 6 artists/engineers built and released 20 Movieoke versions from April ’22 to March ’23 (at which point Movieoke became MovieBot). The 2.0 release was notable: it introduced UGC scenes and a custom character creator. These updates significantly improved the creation funnel (51% more recordings, 17% more exports).<br><br>My role during this time was multifaceted. Externally I was responsible for investor updates and internally, my main duty was as product manager working with our lead engineer and designer on scoping eng/tech art sprints to plan upcoming releases. As an individual contributor, I would focus on process improvements to the code managing our tech art process (Maya to Unity) and handle engineering tasks related to character animation system or Unity UI. I also managed release engineering and publishing app updates to Google Play/App Store after each sprint once a build passed QA. <br><br>In 7 months, we scaled from just an idea to 25k WAU/4k DAU on 322k total installs with an average rating of 4.6 starts on iOS/Android.<br><br><a href=\"https://medium.com/teleportal/now-playing-movieoke-2285d02cfa42\" target=\"_blank\">More information here</a>.",
     "video": [
         ["An YouTube Shorts ad for Movieoke (Made with Movieoke)", "https://www.youtube.com/embed/5C9jzgT89f4"],
         ["YouTube Creator Pankh Khan's Movieoke tutorial", "https://www.youtube.com/embed/zUv1lB26BJw?start=69"],
@@ -101,7 +102,7 @@ export const projectData = [
       "Product Manager",
       "Co-founder"
     ],
-    "description": "Built at Teleportal (the startup I co-founded/built from 2019-2023), MovieBot made AI-assisted content creation accessible for 3D Animated videos. Our team of 4 built upon Movieoke (see project info on this site) to re-launch as MovieBot in just 8 weeks. The core difference between Movieoke and MovieBot is that in the former, creators must record each line of dialogue to make a video. But with MovieBot, creators can enter a topic and make a video instantly, then choose to tweak what is created however they want. The emphasis on speed-to-create led to a 20% month over month growth rate for MovieBot compared to Movieoke.<br><br>During this transition, I became CEO of our startup, but remained focus on building the product as an engineer, technical artist and product manager. In addition to OpenAI gpt-3.5, we integrated other AI tools including Avaturn Avatars and Blockade Labs Skyboxes and ElevenLabs voices into our Unity App. We reached ~1 million installs worldwide and had over 120k MAU and began generating  revenue. Despite our team’s best efforts to build a great product and begin generating revenue, we could not sustain development and the company was shut down at the end of 2023. Fans of MovieBot continue to share old videos using the product today!",
+    "description": "Built at Teleportal (the startup I co-founded/built from 2019-2023), MovieBot built upon Movieoke (see project info on this site) to make an even better UX for content creation in 3D. After an unfortunate 50% reduction in our team size, we were able to conceptualize, build and release MovieBot in just 8 weeks. The core difference between Movieoke and MovieBot is that in the former, creators must record each line of dialogue to make a video. But with MovieBot, creators can enter a topic and make a video instantly, then choose to tweak what gets created however they want. The emphasis on speed-to-create led to a 20% month over month growth rate for MovieBot compared to Movieoke from March to June 2023.<br><br>During this transitory period, I became CEO of our startup but remained focus on building the product as an PM, engineer and technical artist.<br><br>In addition to OpenAI gpt-3.5 for script writing, we integrated three other AI tools into MovieBot including Avaturn Avatars and Blockade Labs Skyboxes and ElevenLabs voices. We reached ~1 million installs worldwide and had over 120k MAU and began generating revenue with our MovieBot+ subscription. <br><br>Despite our team’s best efforts to build a great product and grow our revenue stream, we could not sustain development and the company shut down around January 2024. Fans of MovieBot continue to share old videos using the product today!",
     "video": [  
       ["MovieBot featured on Dr. Phil", "https://www.youtube.com/embed/1nOCuDtxdRo" ],
       ["MovieBot YouTube Shorts ad", "https://www.youtube.com/embed/kVMfdhZznIQ"],
@@ -124,8 +125,8 @@ export const projectData = [
     ],
     "description": "After working with Professor Nugent on previous Digital Humantities projects in VR at Boston College, we decided to take on a more ambitious concept: reimagining Ulysses in VR. We secured URF grant funding to development this project, and recruited 30 students from Boston College, MIT, Northeastern and Berklee School of Music to help us bring it to life. <br><br>The experience breaks the story up logically by chapter and represents each through objects and images. These are translated into interactive scenes that uncover aspects of the story as the player progresses. Featured in Boston Globe, NYTimes, London Times and demoed at literature and technology conferences around the world including Signapore, Dublin, Seattle and New York.<br><br><a href=\"https://www.bc.edu/bc-web/schools/morrissey/departments/computer-science/about/news-and-notes/joycestick/_jcr_content.html\" target=\"_blank\" More information on my work on Joycestick here</a>",
     "video": [
-        ["Gameplay video of Joycestick", "https://www.youtube.com/embed/27N-_M44swM"],
-        ["Joycestick promotional video", "https://www.youtube.com/embed/kwI70wGPlTw"],
+      ["Joycestick promotional video", "https://www.youtube.com/embed/kwI70wGPlTw"],
+      ["Gameplay video of Joycestick", "https://www.youtube.com/embed/27N-_M44swM"],
     ],
     "flickrAlbumUrl": "https://www.flickr.com/photos/200636511@N06/albums/72177720316771765"
   },
@@ -204,6 +205,7 @@ export const projectData = [
     "title": "MiXR Spatial Synth",
     "photo": "../images/project/mixr.jpg",
     "year": 2020,
+    "details": "Hackathon - UE4 app (Valve Index)",
     "subtitle": "MiXR is an audio-visual synthesizer that maps individual movement through space to sound generation. Built at Reality Hack '20 at MIT",
     "role": [
       "Software Engineer",
